7aaa4c30c6a5bc688fd945b2c6a424ba4a2a5d27
GaelMoney <25393@ma-web.nl>
2022-05-09 12:09:19 +0200

    finished ConsoleMon and Skill Opdracht added to github

---
+++ ConsoleMon/ConsoleMon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleMon
+{
+    class ConsoleMon
+    {
+        int hp = 100;
+        int energy = 120;
+        int damage = 5;
+        public int drain = 10;
+        string name = "pikachu";
+
+        internal void TakeDamage(int damage)
+        {
+            hp = hp - damage;
+
+        }
+        internal void DepleteEnergy(int drain)
+        {
+            energy = energy - drain;
+        }
+    }
+}
---
+++ ConsoleMon/Skill.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleMon
+{
+    class Skill
+    {
+        int damage = 10;
+        int energyCost = 25;
+        string name = "LightningBall";
+        internal void UseOn(ConsoleMon target, ConsoleMon caster)
+        {
+            target.TakeDamage(damage);
+            caster.DepleteEnergy(energyCost);
+        }
+    }
+}
